074e7f961f8a344143e7de9d6376b73c356bdc98
Sandro Tonon <santrotonon@Sandros-MacBook-Air.local>
2015-05-10 21:30:08 +0200

    Startseite

--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,4 @@
-.. SQLTutorium documentation master file, created by
-   sphinx-quickstart on Sun May 10 21:15:20 2015.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to SQLTutorium's documentation!
+SQLTutorium
 =======================================
 
 Contents:
--- docs/nbuild/html/searchindex.js
+++ docs/nbuild/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{Content:0,Modulindex:0,Stichwortverzeichnis:0,Such:0},titles:["Welcome to SQLTutorium&#8217;s documentation!"],titleterms:{Indic:0,SQLTutorium:0,Welcom:0,and:0,documentation:0,tabl:0}})
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{Content:0,Modulindex:0,Stichwortverzeichnis:0,Such:0},titles:["SQLTutorium"],titleterms:{Indic:0,SQLTutorium:0,Welcom:[],and:0,documentation:[],tabl:0}})
